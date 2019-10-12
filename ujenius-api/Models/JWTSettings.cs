2c1920fdbe12e77c8d56e6a2b9462f1bdce41d43
Mkhululi Mtshemla <201402344@student.uj.ac.za>
2019-10-12 19:52:51 +0200

    api working with weather controller

---
+++ ujenius-api/Models/JWTSettings.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace ujenius_api.Models
+{
+    public class JWTSettings
+    {
+        [JsonProperty("secret_key")]
+        public string SecretKey { get; set; }
+    }
+}
